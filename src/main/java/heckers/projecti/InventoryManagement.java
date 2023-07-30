44a82777a4730b12fd6de25287766ff11d451f19
YASH TYAGI <76737782+Tyagi2001@users.noreply.github.com>
2023-07-31 01:32:59 +0530

    Add files via upload

---
+++ src/main/java/heckers/projecti/InventoryManagement.java
@@ -0,0 +1,14 @@
+package heckers.projecti;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aarya
+ */
+public class InventoryManagement {
+    
+}
